4d1bf388ab3f6993aa6e428b439f69f3a57447ae
sashapoliti <sas.politi@gmail.com>
2024-05-02 16:15:10 +0200

    print all hotels with other info in main

--- Views/main.php
+++ Views/main.php
@@ -1,3 +1,9 @@
-<main>
-    <h2>main</h2>
+<main class="container">
+    <ul>
+        <?php
+        foreach ($db as $hotel) {
+            echo "<li> $hotel[name] | $hotel[description] | $hotel[parking] | $hotel[vote] | $hotel[distance_to_center] </li>";
+        }
+        ?>
+    </ul>
 </main>
