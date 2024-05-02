a0caee637b64c34695c7a783ddc1638c33d23354
sashapoliti <sas.politi@gmail.com>
2024-05-02 16:25:02 +0200

    add bootstrap with table

--- Views/main.php
+++ Views/main.php
@@ -1,9 +1,23 @@
+<?php
+$template = "";
+foreach ($db as $hotel) {
+    $template .= "<tr> <td>{$hotel['name']}</td> <td>{$hotel['description']}</td> <td>{$hotel['parking']}</td> <td>{$hotel['vote']}</td> <td>{$hotel['distance_to_center']}</td> </tr>";
+}
+?>
+
 <main class="container">
-    <ul>
-        <?php
-        foreach ($db as $hotel) {
-            echo "<li> $hotel[name] | $hotel[description] | $hotel[parking] | $hotel[vote] | $hotel[distance_to_center] </li>";
-        }
-        ?>
-    </ul>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Parking</th>
+                <th scope="col">Vote</th>
+                <th scope="col">Distance to center</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?= $template ?>
+        </tbody>
+    </table>
 </main>
